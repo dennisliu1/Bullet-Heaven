91f4edc30f7f79041ffdf753974c21ab2721a8a1
Dennis Liu <dennisliu12@gmail.com>
2023-08-12 12:36:09 -0400

    use generic hit object

--- game/Player/Attacks/Ice Spear/ice_spear_attack.gd
+++ game/Player/Attacks/Ice Spear/ice_spear_attack.gd
@@ -28,37 +28,3 @@ extends EntityAttack
 
 
 
-
-#var ice_spear = preload("res://Player/Attacks/Ice Spear/ice_spear.tscn")
-#
-### attack stats
-#var icespear_baseammo = 1
-#var icespear_attackspeed = 1.5
-#var icespear_level = 1
-#
-#
-#
-#@onready var icespear_timer = $IceSpearTimer
-#@onready var icespear_attack_timer = $IceSpearTimer/IceSpearAttackTimer
-#
-### runtime variables
-#var icespear_ammo = 1
-#
-#func equip_attack():
-#	icespear_baseammo = spellcard_data
-#
-#func attack():
-#	if icespear_level > 0:
-#		icespear_timer.wait_time = icespear_attackspeed
-#		if icespear_timer.is_stopped():
-#			icespear_timer.start()
-#
-#func _on_ice_spear_timer_timeout():
-#	icespear_ammo += icespear_baseammo
-#	icespear_attack_timer.start()
-#
-#
-#func _on_ice_spear_attack_timer_timeout():
-#	if icespear_ammo > 0:
-#		pass
-
--- game/Utility/entity_hit.gd
+++ game/Utility/entity_hit.gd
@@ -1,12 +1,66 @@
-extends Node
+extends Area2D
 class_name EntityHit
 
-var hit_object : Node
-var hit_properties: SpellCardData
-var hit_behavior : String
+#var hit_object : Node
+#var hit_properties: SpellCardData
+#var hit_behavior : String
+#
+#static var EMPTY_ENTITY_HIT = EntityHit.new()
 
-static var EMPTY_ENTITY_HIT = EntityHit.new()
+@onready var player = get_tree().get_first_node_in_group("player")
 
+@export var enemy_detect_area : Area2D
+
+var entity_hit: EntityHit # stores the hit properties
+
+
+# Hit properties
+@export var speed = 100
+@export var damage = 5
+@export var knockback_amount = 100
+@export var attack_size = 1.0
+@export var attack_hp = 1
+
+## Hit internal properties
+var target = Vector2.ZERO
+var angle = Vector2.ZERO
+
+signal remove_from_array(object)
+
+## Called when the node enters the scene tree for the first time.
+func _ready():
+	angle = global_position.direction_to(target)
+
+	# the ice spear is current 45 degrees, so we compensate by adding 135 degrees
+	# this way, the ice spear is equal to Vector(1, 0)
+	# and faces right
+	rotation = angle.angle() + deg_to_rad(135)
+	
+	# a small animation where the ice spear starts off small and grows into
+	# its full size.
+	# Tween interpolates between two states, shifting from oen to the other.
+	var tween = create_tween()
+	tween.tween_property(self, "scale", Vector2(1,1) * attack_size, 1).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	tween.play()
+
+
+## Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position += angle * speed * delta
+
+## Called by HurtBox, when it hits an enemy.
+## When the ice spear hits the enemy, remove this projectile.
+func enemy_hit(charge = 1):
+	attack_hp -= charge
+	if attack_hp <= 0:
+		_delete_self()
+
+func _delete_self():
+	emit_signal("remove_from_array", self)
+	queue_free()
+
+func _on_life_time_timer_timeout():
+	_delete_self()
 
 
 
