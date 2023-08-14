78945603c3f9db4e88c2b8c518eadd7db20bec6e
Dennis Liu <dennisliu12@gmail.com>
2023-08-13 20:28:42 -0400

    get property modifier working for spellcard evalu

--- game/UI/equipment_container.gd
+++ game/UI/equipment_container.gd
@@ -17,6 +17,8 @@ var attack_instances = {}
 
 var equipment_data
 
+var evaluated_spellcards
+
 ## Called when the node enters the scene tree for the first time.
 func _ready():
 	equipment_data = equipment_inventory_data.items[0]
@@ -54,11 +56,15 @@ func _on_equipment_changed(_indexes):
 
 ## associate all newly added spellcards with the equipment.
 ## They are socketed to the equipment now.
-func _on_spellcard_set(spellcards):
-	for i in range(spellcards.size()):
-		if spellcards[i] is SpellCardData:
-			spellcards[i].equipment = equipment_data
-			_add_action(spellcards[i])
+func _on_spellcard_set(new_spellcards):
+	for i in range(new_spellcards.size()):
+		if new_spellcards[i] is SpellCardData:
+			new_spellcards[i].equipment = equipment_data
+#			_add_action(new_spellcards[i])
+	
+#	_save_spellcard_data()
+	evaluated_spellcards = evaluate_spellcards(spellcard_inventory_data.items)
+	_add_evaluated_spell_attacks()
 
 func _add_action(spellcard):
 	if spellcard.sub_type == ItemData.ITEM_SUB_TYPE.PROJECTILE:
@@ -72,8 +78,12 @@ func _on_spellcard_removed(spellcards):
 	for i in range(spellcards.size()):
 		if spellcards[i] is SpellCardData:
 			spellcards[i].equipment = ItemData.EMPTY_ITEM_DATA
-			player.remove_spellcard(spellcards[i], get_index())
-#			attack_instances[spellcards[i].key].queue_free()
+#			player.remove_spellcard(spellcards[i], get_index())
+	_remove_evaluated_spell_attacks()
+	
+#	_save_spellcard_data()
+	evaluated_spellcards = evaluate_spellcards(spellcard_inventory_data.items)
+	_add_evaluated_spell_attacks()
 
 func display_equipment():
 	equipment_slot.display_item(equipment_data)
@@ -91,13 +101,30 @@ func _save_spellcard_data():
 		for i in range(spellcard_inventory_data.slots):
 			equipment_data.spell_slots[i] = spellcard_inventory_data.items[i]
 
+func _remove_evaluated_spell_attacks():
+	for spellcard in evaluated_spellcards:
+		if spellcard is SpellCardData and spellcard.sub_type == ItemData.ITEM_SUB_TYPE.PROJECTILE:
+			player.remove_spellcard(spellcard, get_index())
+
+func _add_evaluated_spell_attacks():
+	for i in range(evaluated_spellcards.size()):
+		if evaluated_spellcards[i] is SpellCardData:
+			evaluated_spellcards[i].equipment = equipment_data
+			_add_action(evaluated_spellcards[i])
 
 # ---
 
-func evaluate_spellcards(spellcards: Array[SpellCardData]):
+func evaluate_spellcards(spellcards: Array):
 	var stack = []
 	for spellcard in spellcards:
 		var new_spellcard = spellcard.duplicate()
+
+		if spellcard == ItemData.EMPTY_ITEM_DATA:
+			continue
+
+		if stack.size() <= 0:
+			stack.append(new_spellcard)
+			continue
 
 		## the conditions are based on the top card and the spellcard.
 		var is_looping = true
@@ -105,8 +132,9 @@ func evaluate_spellcards(spellcards: Array[SpellCardData]):
 			## if the stack is empty, add the spellcard directly
 			if stack.size() <= 0:
 				stack.append(new_spellcard)
+				is_looping = false
 				continue
-			
+
 			var top_card : SpellCardData = stack[stack.size()-1]
 			var top_card_sub_type = top_card.sub_type
 			if new_spellcard.sub_type == ItemData.ITEM_SUB_TYPE.PROJECTILE:
@@ -121,47 +149,46 @@ func evaluate_spellcards(spellcards: Array[SpellCardData]):
 					apply_modifier_to_spellcard(new_spellcard, top_card)
 					stack.pop_back()
 				elif top_card_sub_type == ItemData.ITEM_SUB_TYPE.ON_FIRE_PROJECTILE_MODIFIER:
-					
+					apply_modifier_to_spellcard(new_spellcard, top_card)
 					stack.pop_back()
-		pass
-	pass
+	return stack
 
 func apply_modifier_to_spellcard(spellcard: SpellCardData, modifier_card: SpellCardData):
-	if modifier_card.has("damage"):
+	if modifier_card.get("damage"):
 		spellcard.damage *= modifier_card.damage
-	if modifier_card.has("damage_shock"):
+	if modifier_card.get("damage_shock"):
 		spellcard.damage_shock *= modifier_card.damage_shock
-	if modifier_card.has("damage_fire"):
+	if modifier_card.get("damage_fire"):
 		spellcard.damage_fire *= modifier_card.damage_fire
-	if modifier_card.has("damage_ice"):
+	if modifier_card.get("damage_ice"):
 		spellcard.damage_ice *= modifier_card.damage_ice
-	if modifier_card.has("damage_poison"):
+	if modifier_card.get("damage_poison"):
 		spellcard.damage_poison *= modifier_card.damage_poison
-	if modifier_card.has("damage_soul"):
+	if modifier_card.get("damage_soul"):
 		spellcard.damage_soul *= modifier_card.damage_soul
-	if modifier_card.has("action_delay"):
+	if modifier_card.get("action_delay"):
 		spellcard.action_delay *= modifier_card.action_delay
-	if modifier_card.has("num_attacks"):
+	if modifier_card.get("num_attacks"):
 		spellcard.num_attacks += modifier_card.num_attacks
-	if modifier_card.has("spread"):
+	if modifier_card.get("spread"):
 		spellcard.spread *= modifier_card.spread
-	if modifier_card.has("velocity"):
+	if modifier_card.get("velocity"):
 		spellcard.velocity *= modifier_card.velocity
-	if modifier_card.has("lifetime"):
+	if modifier_card.get("lifetime"):
 		spellcard.lifetime *= modifier_card.lifetime
-	if modifier_card.has("radius"):
+	if modifier_card.get("radius"):
 		spellcard.radius *= modifier_card.radius
-	if modifier_card.has("knockback"):
+	if modifier_card.get("knockback"):
 		spellcard.knockback *= modifier_card.knockback
-	if modifier_card.has("pierce"):
+	if modifier_card.get("pierce"):
 		spellcard.pierce *= modifier_card.pierce
-	if modifier_card.has("bounce"):
+	if modifier_card.get("bounce"):
 		spellcard.bounce *= modifier_card.bounce
-	if modifier_card.has("hit_hp"):
+	if modifier_card.get("hit_hp"):
 		spellcard.hit_hp *= modifier_card.hit_hp
-	if modifier_card.has("hit_size"):
+	if modifier_card.get("hit_size"):
 		spellcard.hit_size *= modifier_card.hit_size
-	if modifier_card.has("on_fire_effect"):
+	if modifier_card.get("on_fire_effect"):
 		spellcard.on_fire_effect = []
 		for spellcard_effect in modifier_card.on_fire_effect:
 			spellcard.on_fire_effect.append(spellcard_effect.duplicate())
