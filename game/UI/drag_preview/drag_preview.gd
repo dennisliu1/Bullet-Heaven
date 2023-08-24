79d804e4fb8306e52be6f311100f71b0fe4dc4c2
Dennis Liu <dennisliu12@gmail.com>
2023-08-23 21:39:00 -0400

    updated inventory drag & drop

--- game/UI/drag_preview/drag_preview.gd
+++ game/UI/drag_preview/drag_preview.gd
@@ -6,7 +6,6 @@ var dragged_item = ItemData.EMPTY_ITEM_DATA : set = set_dragged_item
 @export var inventory_menu_array : Array[SlotContainer]
 @export var inventory_array : Array[InventoryData]
 @export var tooltip : ColorRect
-var inventory_visible = false
 
 @onready var item_icon = $ItemIcon
 @onready var item_quantity = $ItemQuantity
@@ -19,14 +18,6 @@ func _ready():
 		if item_slot.inventory_index >= 0:
 			index = item_slot.inventory_index
 		item_slot.gui_input.connect(_on_ItemSlot_gui_input.bind(item_slot.inventory_data, index))
-#		item_slot.connect("gui_input", self, "_on_ItemSlot_gui_input", [index])
-	
-	# default hide the inventory menus
-	for i in range(inventory_menu_array.size()):
-		inventory_menu_array[i].visible = false
-	for i in range(equipment_containers.size()):
-		equipment_containers[i].visible = false
-	pass
 
 func _process(_delta):
 	if dragged_item:
@@ -42,26 +33,25 @@ func set_dragged_item(item):
 		item_icon.texture = null
 		item_quantity.text = ""
 
-func _unhandled_input(event):
-	if event.is_action_pressed("show_inventory_menu"):
-		# if the player is dragging an item around, don't close the inventory
-		if inventory_visible and dragged_item != ItemData.EMPTY_ITEM_DATA:
-			return
-
-		for i in range(inventory_menu_array.size()):
-			inventory_menu_array[i].visible = !inventory_menu_array[i].visible
-		for i in range(equipment_containers.size()):
-			equipment_containers[i].visible = !equipment_containers[i].visible
-		inventory_visible = !inventory_visible
-		
-#		get_parent().paused = !get_parent().paused
-#		hide_tooltip()
+#func _unhandled_input(event):
+#	if event.is_action_pressed("show_inventory_menu"):
+#		# if the player is dragging an item around, don't close the inventory
+#		if inventory_visible and dragged_item != ItemData.EMPTY_ITEM_DATA:
+#			return
+#
+#		for i in range(inventory_menu_array.size()):
+#			inventory_menu_array[i].visible = !inventory_menu_array[i].visible
+#		for i in range(equipment_containers.size()):
+#			equipment_containers[i].visible = !equipment_containers[i].visible
+#		inventory_visible = !inventory_visible
+#
+##		get_parent().paused = !get_parent().paused
+##		hide_tooltip()
 
 func _on_ItemSlot_gui_input(event, inventory_data, index):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			if inventory_visible:
-				drag_item(inventory_data, index)
+			drag_item(inventory_data, index)
 #				hide_tooltip()
 
 func drag_item(inventory_data, index):
